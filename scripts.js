b0f71ed7f0e5d704036812aae1a8e56cc230425d
CarlFecito <cmolinaf96@gmail.com>
2022-05-14 06:45:22 -0400

    add function to create buttons

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function addNumbers() {
+    for (i = 0; i <= 9; i++) {
+        addDiv(i, 'numbers')
+    }
+}
+
+
+function addDiv(num, cont) {
+    const box = document.getElementById(cont)
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", num)
+    newDiv.innerHTML = num
+    newDiv.setAttribute("class", 'square')
+    box.appendChild(newDiv)
+}
