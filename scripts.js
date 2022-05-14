260a9e0a898727468d62e3ad4bc3dc001830b79f
CarlFecito <cmolinaf96@gmail.com>
2022-05-14 18:53:26 -0400

    basic scripts finished

--- scripts.js
+++ scripts.js
@@ -1,23 +1,44 @@
 let screenContent = ''
-addNumbers()
+let cache = 0
+drawNumbers()
 
+const clearButton = document.getElementById('C')
+clearButton.addEventListener('click', () => {
+    clear()
+})
 
-function addNumbers() {
+const selectNumbers = document.querySelectorAll('.nmbr')
+selectNumbers.forEach((button => {
+    button.addEventListener('click', () => {
+        displayInput(button.innerText)
+    })
+}))
+
+let calcScreen = document.querySelector('#screen')
+function displayInput(inpt) {
+    screenContent = screenContent + inpt
+    calcScreen.innerText = screenContent    
+}
+
+function drawNumbers() {
     for (let i = 1; i <= 9; i++) {
-        addButton(i)
+        drawButton(i)
     }
 }
 
-function addButton(num) {
+function drawButton(num) {
     const box = document.getElementById('numbers')
     const newButton = document.createElement("button");
     newButton.setAttribute("id", 'btn' + num)
     newButton.innerHTML = num
-    newButton.setAttribute("class", 'square')
+    newButton.setAttribute("class", 'square nmbr')
     box.appendChild(newButton)
 }
 
-function display(input) {
-    const screen = document.getElementById('screen')
-    let screenContent = screenContent + input
+function clear() {
+    calcScreen.innerText = 0
 }
+
+/* para las operaciones usar un array 0, operaciones
+                                #1  , operaciones   
+                                #2 , operaciones */
