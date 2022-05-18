31e91e1b04f9daec8fc6c02dae8527a97b603936
CarlFecito <cmolinaf96@gmail.com>
2022-05-18 19:30:51 -0400

    kinda works

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
+let calcScreen = document.querySelector('#screen')
 let screenContent = ''
-let cache = 0
+let actualNumber = 0
+let previousNumber = ''
+let actualOperation = ''
+let prevOperation = 'skip'
+let result
+clear()
+
+function clear() {
+    calcScreen.innerText = 0
+    screenContent = ''
+    actualNumber = 0
+    previousNumber = ''
+    actualOperation = ''
+    prevOperation = 'skip'
+}
 
 const clearButton = document.getElementById('c')
 clearButton.addEventListener('click', () => {
@@ -10,7 +25,6 @@ const selectNumbers = document.querySelectorAll('#nmbr')
 selectNumbers.forEach((button => {
     button.addEventListener('click', () => {
         displayInput(button.innerText)
-        console.log('warever')
     })
 }))
 
@@ -19,59 +33,67 @@ function displayInput(inpt) {
     display(screenContent)    
 }
 
-let calcScreen = document.querySelector('#screen')
-
 function display(content) {
     calcScreen.innerText = content
 }
 
-function drawNumbers() {
-    for (let i = 1; i <= 9; i++) {
-        drawButton(i)
-    }
-}
-
-function drawButton(num) {
-    const box = document.getElementById('numbers')
-    const newButton = document.createElement("button");
-    newButton.setAttribute("id", 'btn' + num)
-    newButton.innerHTML = num
-    newButton.setAttribute("class", 'square nmbr')
-    box.appendChild(newButton)
-}
-
-function clear() {
-    calcScreen.innerText = 0
-    screenContent = ''
-    cache = 0
-    operation = 0
-}
-
-let operation = 0
-
-function operator(opr) {
-    screenContent = Number(screenContent)
-    if (operation === '/' && screenContent != 0 && screenContent != '') {
-        cache = cache / screenContent
-    } else if (operation === '+' || operation === 0) {
-        cache = cache + screenContent
-    } else if (operation === '-') {
-        cache = cache - screenContent
-    } else if (operation === '*') {
-        cache = cache * screenContent
-    } else {
-        alert('error')
-    }
-    operation = opr
-    display(cache)
-}
-
-
-
-const oprButtons = document.querySelectorAll('#opr')
+const oprButtons = document.querySelectorAll('.opr')
 oprButtons.forEach((button => {
     button.addEventListener('click', () => {
-        operator(button.innerText)
+        operator(button.id)
     })
 }))
 
+function operator(opr) {
+    actualNumber = Number(screenContent)
+    console.log(prevOperation)
+    if (opr === 'equal') {
+        screenContent = ''
+        calculate(previousNumber, actualNumber, prevOperation)
+        prevOperation = 'skip'
+        
+    } else if (prevOperation === 'skip') {
+        previousNumber = actualNumber
+        prevOperation = opr
+        console.log(prevOperation)
+        calcScreen.innerText = previousNumber
+        screenContent = ''
+    } else {
+        actualOperation = opr
+        calcScreen.innerText = 0
+        screenContent = ''
+        calculate(previousNumber, actualNumber, prevOperation)
+        prevOperation = actualOperation
+        
+    }
+}
+
+function calculate(number1, number2, operation1) {
+    console.log(operation1)
+    if (operation1 === 'divide' && number2 === 0) {
+        alert(`can't divide by zero`)
+    } else if (operation1 === 'divide') {
+        result = number1 / number2
+        console.log(result)
+        end(result)
+    } else if (operation1 === 'mult') {
+        result = number1 * number2
+        console.log(result)
+        end(result)
+    } else if (operation1 === 'rest') {
+        result = number1 - number2
+        console.log(result)
+        end(result)
+    } else if (operation1 === 'sum') {
+        result = number1 + number2
+        console.log(result)
+        end(result)
+    } else {
+        console.log('error')
+    }
+}
+
+function end(num) {
+    calcScreen.innerText = num
+    previousNumber = num
+}
